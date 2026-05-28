4a6eb242799c3ac78efe6982756356035f647fb1
hectortoledo <hectortoledo297@gmail.com>
2026-05-28 12:19:26 +0200

    Fix: guardado definitivo de tablename plans en plural

--- app/models/plan.py
+++ app/models/plan.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 class Plan(SQLModel, table=True):
-    __tablename__ = "plan"
+    __tablename__ = "plans"
 
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True, index=True)
