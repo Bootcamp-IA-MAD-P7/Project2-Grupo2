37417b1d8180659ae209e214e73d5bb1c3c6926a
anahi-am <anahiamerisea@gmail.com>
2026-05-29 13:19:42 +0200

    first commit

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.plan import Plan
-from app.models.member import Member
-from app.models.membership import Membership
-from app.models.payment import Payment
-from app.models.attendance import Attendance
-from app.models.shift import AvailableShift
-from app.models.reservation import Reservation
